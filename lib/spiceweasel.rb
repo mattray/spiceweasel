32d15f38aab72ef91a7a9e08cdc99f190e98f5df
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2011-07-14 13:14:15 +0100

    Extract environment list.

--- lib/spiceweasel.rb
+++ lib/spiceweasel.rb
@@ -2,4 +2,5 @@ module Spiceweasel
   autoload :CLI, 'spiceweasel/cli'
   autoload :RunList, 'spiceweasel/run_list'
   autoload :CookbookList, 'spiceweasel/cookbook_list'
+  autoload :EnvironmentList, 'spiceweasel/environment_list'
 end
