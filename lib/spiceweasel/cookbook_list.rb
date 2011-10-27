59d5a4d83cff33e71876a14a4b51262903c68eac
mattray <matthewhray@gmail.com>
2011-10-27 17:33:00 -0500

    fix for issue #6, catch empty cookbooks, environments, roles, data bags and nodes.

--- lib/spiceweasel/cookbook_list.rb
+++ lib/spiceweasel/cookbook_list.rb
@@ -2,32 +2,34 @@ class Spiceweasel::CookbookList
   def initialize(cookbooks = [], options = {})
     @create = @delete = ''
     @cookbooks = []
-    cookbooks.each do |cookbook|
-      cb = cookbook.keys.first
-      if cookbook[cb] and cookbook[cb].length > 0
-        version = cookbook[cb][0].to_s || ""
-        args = cookbook[cb][1] || ""
-      end
-      STDOUT.puts "DEBUG: cookbook: #{cb} #{version}" if DEBUG
-      @delete += "knife cookbook#{options['knife_options']} delete #{cb} #{version} -y\n"
-      if File.directory?("cookbooks")
-        if version and File.directory?("cookbooks/#{cb}")
-          #check metadata.rb for requested version
-          metadata = File.open("cookbooks/#{cb}/metadata.rb").grep(/^version/)[0].split()[1].gsub(/"/,'').to_s
-          if (metadata != version)
-            raise "Invalid version #{version} of '#{cb}' requested, #{metadata} is already in the cookbooks directory."
-            exit(-1)
-          end
-        elsif !File.directory?("cookbooks/#{cb}")
-          @create += "knife cookbook#{options['knife_options']} site download #{cb} #{version} --file cookbooks/#{cb}.tgz #{args}\n"
-          @create += "tar -C cookbooks/ -xf cookbooks/#{cb}.tgz\n"
+    if cookbooks
+      cookbooks.each do |cookbook|
+        cb = cookbook.keys.first
+        if cookbook[cb] and cookbook[cb].length > 0
+          version = cookbook[cb][0].to_s || ""
+          args = cookbook[cb][1] || ""
         end
-      else
-        STDERR.puts "cookbooks directory not found, validation and downloading skipped"
-      end
-      @create += "knife cookbook#{options['knife_options']} upload #{cb}\n"
+        STDOUT.puts "DEBUG: cookbook: #{cb} #{version}" if DEBUG
+        @delete += "knife cookbook#{options['knife_options']} delete #{cb} #{version} -y\n"
+        if File.directory?("cookbooks")
+          if version and File.directory?("cookbooks/#{cb}")
+            #check metadata.rb for requested version
+            metadata = File.open("cookbooks/#{cb}/metadata.rb").grep(/^version/)[0].split()[1].gsub(/"/,'').to_s
+            if (metadata != version)
+              raise "Invalid version #{version} of '#{cb}' requested, #{metadata} is already in the cookbooks directory."
+              exit(-1)
+            end
+          elsif !File.directory?("cookbooks/#{cb}")
+            @create += "knife cookbook#{options['knife_options']} site download #{cb} #{version} --file cookbooks/#{cb}.tgz #{args}\n"
+            @create += "tar -C cookbooks/ -xf cookbooks/#{cb}.tgz\n"
+          end
+        else
+          STDERR.puts "cookbooks directory not found, validation and downloading skipped"
+        end
+        @create += "knife cookbook#{options['knife_options']} upload #{cb}\n"
 
-      @cookbooks << cb
+        @cookbooks << cb
+      end
     end
   end
 
