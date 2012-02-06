10c431117e9b2c10aae1559383b914f0f3b87d21
mattray <matthewhray@gmail.com>
2012-02-06 10:58:38 -0600

    formatting cleanup

--- lib/spiceweasel/cookbook_parser.rb
+++ lib/spiceweasel/cookbook_parser.rb
@@ -1,25 +1,25 @@
 class Spiceweasel::CookbookParser
 
   attr_accessor :_name, :_dependencies, :_version
-   
+
   def initialize(file_name)
     @_name = file_name.split('/').last
     @_dependencies = []
     @_version = ""
     @file_name = file_name
   end
-  
+
   def parse
     return nil unless Dir.exists?("cookbooks/#{@file_name}")
-    
+
     begin
       file = File.new("cookbooks/#{@file_name}/metadata.rb", "r")
-    rescue 
+    rescue
       STDERR.puts "WARNING: Could not retrieve cookbook information: #{@file_name}"
       return nil
     end
-    
-    while (line = file.gets) 
+
+    while (line = file.gets)
       if line.start_with?("name") || line.start_with?("version") || line.start_with?("depends")
         begin
           eval line
@@ -29,17 +29,17 @@ class Spiceweasel::CookbookParser
       end
     end
     file.close
-    
+
   end
-  
+
   def name(*args)
     @_name = args.shift
   end
-  
+
   def version(*args)
     @_version = args.shift
   end
-    
+
   def depends(*args)
     cookbook = args.shift
     if args.length > 0
@@ -47,5 +47,5 @@ class Spiceweasel::CookbookParser
     end
     @_dependencies << {:cookbook => cookbook, :version => cookbook_version}
   end
-  
+
 end
--- lib/spiceweasel/directory_extractor.rb
+++ lib/spiceweasel/directory_extractor.rb
@@ -2,7 +2,7 @@ class Spiceweasel::DirectoryExtractor
 
   def self.parse_objects
     objects = {"cookbooks" => nil, "roles" => nil, "environments" => nil, "data bags" => nil, "nodes" => nil}
-    
+
     # COOKBOOKS
     cookbooks = []
     Dir.glob("cookbooks/*").each do |cookbook_full_path|
@@ -13,15 +13,15 @@ class Spiceweasel::DirectoryExtractor
     end
     cookbook_names = self.order_cookbooks_by_dependency cookbooks
     objects["cookbooks"] = cookbook_names unless cookbook_names.empty?
-    
+
     # ROLES
-    roles = []    
+    roles = []
     Dir.glob("roles/*.{rb,json}").each do |role_full_path|
       role = self.grab_name_from_path role_full_path
       roles << {role => nil}
     end
     objects["roles"] = roles unless roles.nil?
-    
+
     # ENVIRONMENTS
     environments = []
     Dir.glob("environments/*.{rb,json}").each do |environment_full_path|
@@ -29,7 +29,7 @@ class Spiceweasel::DirectoryExtractor
       environments << {environment => nil}
     end
     objects["environments"] = environments unless environments.empty?
-      
+
     # DATA BAGS
     data_bags = []
     Dir.glob("data_bags/*").each do |data_bag_full_path|
@@ -41,7 +41,7 @@ class Spiceweasel::DirectoryExtractor
       data_bags << {data_bag => data_bag_items} unless data_bag_items.empty?
     end
     objects["data bags"] = data_bags unless data_bags.empty?
-      
+
     # NODES
     # TODO: Cant use this yet as node_list.rb doesnt support node from file syntax but expects the node info to be part of the objects passed in
     # nodes = []
@@ -50,14 +50,14 @@ class Spiceweasel::DirectoryExtractor
     #   nodes  << {node => nil}
     # end
     # objects["nodes"] = nodes unless nodes.empty?
-        
+
     objects
   end
-  
+
   def self.grab_filename_from_path path
     path.split('/').last
   end
-  
+
   def self.grab_name_from_path path
     name = self.grab_filename_from_path(path).split('.')
     if name.length>1
@@ -65,7 +65,7 @@ class Spiceweasel::DirectoryExtractor
     end
     name.join('.')
   end
-  
+
   def self.order_cookbooks_by_dependency cookbooks
 
     # Weak algorithm, not particularly elegant, ignores version info as unlikely to have two versions of a cookbook anyway
@@ -79,7 +79,7 @@ class Spiceweasel::DirectoryExtractor
       unsorted_cookbooks = left_to_sort
       left_to_sort = []
       num_sorted_cookbooks_this_iteration
-      
+
       unsorted_cookbooks.each do |cookbook|
         name = cookbook[:name]
         dependencies = cookbook[:dependencies]
@@ -94,33 +94,33 @@ class Spiceweasel::DirectoryExtractor
 
         dependencies_satisfied_yet = true
         dependencies.each {|dependency| dependencies_satisfied_yet = false unless ordered_cookbooks.include? dependency[:cookbook]}
-      
+
         if dependencies_satisfied_yet
           ordered_cookbooks << name
           num_sorted_cookbooks_this_iteration += 1
           next
         end
-      
+
         left_to_sort << cookbook
 
       end
-      
+
     end
-    
+
     if left_to_sort.size > 0
       STDERR.puts "ERROR: Dependencies not satisfied or circular dependencies between cookbooks: #{left_to_sort}"
       exit(-1)
     end
-    
+
     output_cookbooks = []
     ordered_cookbooks.each do |cookbook|
       output_cookbooks << {cookbook => nil}
     end
     output_cookbooks
-        
+
   end
-  
+
 end
 
 
-          
+
