ed75565987ccaa59846dc9fa2eb1e47535ebac63
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-17 16:00:10 -0800

    Add support for Berkshelf

---
+++ lib/spiceweasel/berksfile.rb
@@ -0,0 +1,32 @@
+require 'berkshelf'
+
+module Spiceweasel
+  class Berksfile
+
+    attr_reader :create
+    attr_reader :delete
+
+    def initialize(berkshelf=nil)
+      @create = []
+      @delete = []
+      unless(berkshelf.nil?)
+        case berkshelf
+        when String
+          path = berkshelf
+        when Hash
+          path = berkshelf['path']
+          berks_options = " #{berkshelf['options']}"
+        end
+        path ||= './Berksfile'
+        berks = Berkshelf::Berksfile.from_file(path)
+        @create << "berks upload#{berks_options}"
+        Berkshelf.ui.mute do
+          berks.resolve.each do |cb|
+            @delete << "knife cookbook#{Spiceweasel::Config[:knife_options]} delete #{cb.cookbook_name} #{cb.version} -a -y"
+          end
+        end
+      end
+    end
+
+  end
+end
