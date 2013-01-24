c67a6260f8eaf1fa839237e1a942b985b59bc2f2
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-24 07:27:08 -0800

    Provide path in cli command. Cache cookbook name/versions

--- lib/spiceweasel/berksfile.rb
+++ lib/spiceweasel/berksfile.rb
@@ -3,26 +3,31 @@ module Spiceweasel
 
     attr_reader :create
     attr_reader :delete
+    attr_reader :cookbook_list
 
     def initialize(berkshelf=nil)
       @create = []
       @delete = []
+      @cookbook_list = {}
       unless(berkshelf.nil?)
         # only load berkshelf if we are going to use it
         require 'berkshelf'
 
+        berks_options = []
         case berkshelf
         when String
           path = berkshelf
         when Hash
           path = berkshelf['path']
-          berks_options = " #{berkshelf['options']}"
+          berks_options << berkshelf['options'] if berkshelf['options']
         end
+        berks_options << "-b #{path}"
         path ||= './Berksfile'
         berks = Berkshelf::Berksfile.from_file(path)
-        @create << "berks upload#{berks_options}"
+        @create << "berks upload #{berks_options.join(' ')}"
         Berkshelf.ui.mute do
           berks.resolve.each do |cb|
+            @cookbook_list[cb.cookbook_name] = cb.version
             @delete << "knife cookbook#{Spiceweasel::Config[:knife_options]} delete #{cb.cookbook_name} #{cb.version} -a -y"
           end
         end
