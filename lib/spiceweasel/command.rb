2aa38d473e40ee8842a9f5e182756ba6c81a1267
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-24 09:09:00 -0800

    Allow failure by default to keep current behavior consistent

--- lib/spiceweasel/command.rb
+++ lib/spiceweasel/command.rb
@@ -7,7 +7,7 @@ module Spiceweasel
     def initialize(command, options={})
       @command = command.rstrip
       @options = options
-      @allow_failure = options['allow_failure']
+      @allow_failure = options.has_key?('allow_failure') ? options['allow_failure'] : true
     end
 
     alias_method :allow_failure?, :allow_failure
