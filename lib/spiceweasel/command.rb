bef0eb96af1fda7db2c364a74b82d6be8e42ab51
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-24 08:55:59 -0800

    Provide Command class to hold commands. Add helper for creating arrays of create and delete commands.

---
+++ lib/spiceweasel/command.rb
@@ -0,0 +1,17 @@
+module Spiceweasel
+  class Command
+
+    attr_reader :allow_failure
+    attr_reader :command
+    
+    def initialize(command, options={})
+      @command = command
+      @options = options
+      @allow_failure = options['allow_failure']
+    end
+
+    alias_method :allow_failure?, :allow_failure
+    alias_method :to_s, :command
+
+  end
+end
---
+++ lib/spiceweasel/command_helper.rb
@@ -0,0 +1,13 @@
+module Spiceweasel
+  module CommandHelper
+    def create_command(*args)
+      @create ||= []
+      @create.push(Command.new(*args))
+    end
+
+    def delete_command(*args)
+      @delete ||= []
+      @delete.push(Command.new(*args))
+    end
+  end
+end
