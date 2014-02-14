afd8d43800159aba32cc1e88365e4ff09e995b6c
Matt Ray <matthewhray@gmail.com>
2014-02-14 08:33:11 -0800

    rspec cleanups

--- test/spec/joyent-vsphere_spec.rb
+++ test/spec/joyent-vsphere_spec.rb
@@ -1,7 +1,8 @@
-# Test new 2.3 functionality
-describe 'testing 2.3 functionality' do
-  before(:each) do
-    @expected_output = <<-OUTPUT
+require 'mixlib/shellout'
+
+describe '2.3 functionality' do
+  it "knife-joyent, knife-vsphere and --bulkdelete functionality from 2.3" do
+    expected_output = <<-OUTPUT
 knife cookbook delete apache2  -a -y
 knife environment delete qa -y
 knife role delete base -y
@@ -14,12 +15,16 @@ knife role from file base.rb
 seq 2 | parallel -u -j 0 -v "knife joyent server create -i ~/.ssh/joyent.pem -E qa -r 'role[base]'"
 seq 2 | parallel -u -j 0 -v "knife vsphere vm clone -P secret_password -x Administrator --template some_template -r 'role[base]'"
     OUTPUT
-
-    @spiceweasel_binary = File.join(File.dirname(__FILE__), *%w[.. .. bin spiceweasel])
-  end
-
-  it "test joyent, vsphere and --bulkdelete functionality from 2.3" do
-    `#{@spiceweasel_binary} --parallel --bulkdelete -r --novalidation test/examples/joyent-vsphere-example.yml`.should == @expected_output
+    spiceweasel_binary = File.join(File.dirname(__FILE__), *%w[.. .. bin spiceweasel])
+    spcwsl = Mixlib::ShellOut.new(spiceweasel_binary,
+      '--parallel',
+      '--bulkdelete',
+      '-r',
+      '--novalidation',
+      'test/examples/joyent-vsphere-example.yml',
+      :environment => {'PWD' => "#{ENV['PWD']}/test/chef-repo"} )
+    spcwsl.run_command
+    expect(spcwsl.stdout).to eq expected_output
   end
 
 end
